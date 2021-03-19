24ed4e6bccf37b81052e973d71dc6f04abc67987
Angel V <angeelvallem@gmail.com>
2021-03-18 18:59:26 -0600

    first build for prodcution version 1.0

--- src/App.vue
+++ src/App.vue
@@ -2,76 +2,118 @@
   <div>
     <h1 class="text-center t">Welcome to your simple Web CheckLists</h1>
 
-    <div class="container">
+    <div class="container-fluid container-lg">
       <div class="row">
-        <div
-          class="col-12 col-md-5 col-lg-5 my-2 m-md-2 border shadow-sm rounded p-0"
-          v-for="(List, ListIndex) in todosList"
-          :key="ListIndex"
-        >
-          <h2 class="text-center">{{ List.ListName}} {{ListIndex}}</h2>
-          <ul
-            class="d-flex p-0 d-flex justify-content-between"
-            v-for="(items, index) in List.todos"
-            :key="index"
+        <div class="d-flex flex-wrap justify-content-center">
+          <div
+            class="col-12 col-md-6 col-lg-5 m-2 border shadow-sm rounded p-0"
+            v-for="(List, ListIndex) in todosList"
+            :key="ListIndex"
           >
-            <li class="mx-2" v-show="items.check === false">
-              <input
-                type="checkbox"
-                v-model="items.check"
-                class="form-check-input mx-2"
-              />
-              <label class="form-check-label"></label>
-              {{ items.item }}  
-              
-            </li>
-            <li
-              class="mx-2"
-              v-show="items.check === true"
-              style="text-decoration: line-through"
+            <h2 class="text-center">{{ List.ListName }} {{ ListIndex }}</h2>
+            <ul
+              class="d-flex p-0 d-flex justify-content-between"
+              v-for="(items, index) in List.todos"
+              :key="index"
             >
-              <input
-                type="checkbox"
-                v-model="items.check"
-                class="form-check-input mx-2"
-              />
-              <label class="form-check-label"></label>
-              {{ items.item }} 
-            </li>
-            <div>
-              <button class="btn btn-warning shadow-sm" >
-                <i class="far fa-edit"></i>
-              </button>
-              <button class="btn btn-danger mx-2 shadow-sm" v-on:click="removeItem(ListIndex,index)">
-                <i class="fas fa-minus"></i>
+              <li class="mx-2" v-show="items.check === false">
+                <input
+                  type="checkbox"
+                  v-model="items.check"
+                  class="form-check-input mx-2"
+                />
+                <label class="form-check-label"></label>
+                {{ items.item }}
+              </li>
+              <li
+                class="mx-2"
+                v-show="items.check === true"
+                style="text-decoration: line-through"
+              >
+                <input
+                  type="checkbox"
+                  v-model="items.check"
+                  class="form-check-input mx-2"
+                />
+                <label class="form-check-label"></label>
+                {{ items.item }}
+              </li>
+              <div>
+                <button
+                  class="btn btn-danger mx-2 shadow-sm"
+                  v-on:click="removeItem(ListIndex, index)"
+                >
+                  <i class="fas fa-minus"></i>
+                </button>
+              </div>
+            </ul>
+            <div class="d-flex w-100 justify-content-center">
+              <label for="new-todo"></label>
+              <div class="input-group flex-nowrap p-3">
+                <span class="input-group-text" id="addon-wrapping"
+                  ><i class="far fa-smile"></i
+                ></span>
+                <input
+                  type="text"
+                  class="form-control new-todo"
+                  placeholder="New TODO :D"
+                  aria-label="newTodo"
+                  aria-describedby="addon-wrapping"
+                  v-model="List.newItem"
+                  v-on:keyup.enter="addTodo(ListIndex)"
+                />
+              </div>
+              <button
+                class="btn btn-success m-1 my-3 shadow-sm"
+                v-on:click="addTodo(ListIndex)"
+              >
+                <i class="fas fa-plus">todo</i>
               </button>
             </div>
-          </ul>
-          <div class="d-flex w-100 justify-content-center">
-            <label for="new-todo"></label>
-    <div class="input-group flex-nowrap p-3">
-  <span class="input-group-text" id="addon-wrapping"><i class="far fa-smile"></i></span>
-  <input type="text"  class="form-control new-todo" placeholder="New TODO :D" aria-label="newTodo" aria-describedby="addon-wrapping" v-model="List.newItem">
-</div>
-            <button class="btn btn-success m-1 my-3 shadow-sm" v-on:click="addTodo(ListIndex)">
-              <i class="fas fa-plus">todo</i>
-            </button>
+            <div class="d-flex w-100 justify-content-center">
+              <button
+                class="btn btn-danger m-1 my-3 shadow-sm"
+                v-on:click="deleteList(ListIndex)"
+              >
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            </div>
           </div>
-          <div class="d-flex w-100 justify-content-center">
-            <button
-              class="btn btn-danger m-1 my-3 shadow-sm"
-              v-on:click="deleteList(ListIndex)"
-            >
-              <i class="fas fa-trash-alt"></i>
-            </button>
+          <div
+            class="col-12 col-md-6 col-lg-5 m-2 border shadow-sm rounded p-0 d-flex justify-content-center align-items-center"
+          >
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label"
+                  >Email address</label
+                >
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+                <div id="emailHelp" class="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label"
+                  >Password</label
+                >
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                />
+              </div>
+              <div class="mb-3 form-check">
+              </div>
+              <button class="h-35 btn btn-success m-1 my-3 shadow-sm">
+                <i class="fas fa-plus"></i>
+              </button>
+            </form>
           </div>
-        </div>
-        <div
-          class="col-12 col-md-5 col-lg-1 my-2 m-md-2 border shadow-sm rounded p-5 d-flex justify-content-center align-items-center"
-        >
-            <button class="btn btn-success m-1 my-3 shadow-sm">
-              <i class="fas fa-plus"></i>
-            </button>
         </div>
       </div>
     </div>
@@ -86,7 +128,7 @@ export default {
       todosList: [
         {
           ListName: "Bugs",
-          newItem : '',
+          newItem: "",
           todos: [
             {
               id: Math.random(),
@@ -107,7 +149,7 @@ export default {
         },
         {
           ListName: "House",
-          newItem : '',
+          newItem: "",
           todos: [
             {
               id: Math.random(),
@@ -128,7 +170,7 @@ export default {
         },
         {
           ListName: "House todos",
-          newItem : '',
+          newItem: "",
           todos: [
             {
               id: Math.random(),
@@ -149,7 +191,7 @@ export default {
         },
         {
           ListName: "Market List",
-          newItem : '',
+          newItem: "",
           todos: [
             {
               id: Math.random(),
@@ -175,18 +217,18 @@ export default {
     deleteList: function (index) {
       this.todosList.splice(index, 1);
     },
-    removeItem : function(listIndex,index){
-           this.todosList[listIndex].todos.splice(index,1)
+    removeItem: function (listIndex, index) {
+      this.todosList[listIndex].todos.splice(index, 1);
+    },
+    addTodo: function (listIndex) {
+      this.todosList[listIndex].todos.push({
+        id: Math.random(),
+        item: this.todosList[listIndex].newItem,
+        check: false,
+      });
+    },
   },
-  addTodo : function(listIndex){
-    this.todosList[listIndex].todos.push({
-      id : Math.random(),
-      item : this.todosList[listIndex].newItem,
-      check : false
-    })
-  }
-}
-}
+};
 </script>
 
 <style>
